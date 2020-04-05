f823b45dffae42276dc8f8fafe1cd8113e903ae5
Simon Legner <Simon.Legner@gmail.com>
2020-04-05 15:09:49 +0200

    Build JSON file for taginfo projects

--- build.js
+++ build.js
@@ -29,13 +29,28 @@ data.sort((i, j) => i.key.localeCompare(j.key));
 console.log(`Writing ${data.length} rules to index.json`);
 fs.writeFileSync("index.json", JSON.stringify(data, undefined, 2));
 
+const now = new Date();
+now.setMinutes(0, 0, 0);
 const package = JSON.parse(fs.readFileSync("package.json"));
-package.version = new Date()
-  .toISOString()
-  .substring(0, 10)
-  .replace(/-/g, ".");
+package.version = now.toISOString().substring(0, 10).replace(/-/g, ".");
 console.log(`Updating package version to ${package.version}`);
 fs.writeFileSync("package.json", JSON.stringify(package, undefined, 2));
+
+const author = package.author.match(/(.*) <(.*)>/);
+const taginfo = {
+  data_format: 1,
+  data_updated: now.toISOString().replace(/-|:|\.\d{3}/g, ""),
+  project: {
+    name: package.name,
+    description: package.description,
+    project_url: package.homepage,
+    contact_name: author[1],
+    contact_email: author[2],
+  },
+  tags: data.map(({ key, url }) => ({ key, description: url })),
+};
+console.log(`Updating taginfo.json`);
+fs.writeFileSync("taginfo.json", JSON.stringify(taginfo, undefined, 2));
 
 function sparql(url, filename) {
   return curl(
